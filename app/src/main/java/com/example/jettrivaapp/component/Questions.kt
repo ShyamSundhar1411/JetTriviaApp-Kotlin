1d49efe262ca475802e631cc306ee53430acbf73
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-06-11 14:02:49 +0530

    feat: added Answer Row UI

--- app/src/main/java/com/example/jettrivaapp/component/Questions.kt
+++ app/src/main/java/com/example/jettrivaapp/component/Questions.kt
@@ -3,21 +3,34 @@ package com.example.jettrivaapp.component
 import android.graphics.Paint.Align
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -29,6 +42,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettrivaapp.model.QuestionItem
 import com.example.jettrivaapp.util.AppColors
 import com.example.jettrivaapp.viewmodel.QuestionViewModel
 
@@ -36,19 +50,34 @@ import com.example.jettrivaapp.viewmodel.QuestionViewModel
 @Composable
 fun Questions(viewModel: QuestionViewModel,modifier: Modifier = Modifier){
     val questions = viewModel.getData().value.data?.toMutableList()
-
+    val indexState = remember {
+        mutableIntStateOf(0)
+    }
     if (viewModel.getData().value.loading == true){
         CircularProgressIndicator()
     }else{
 
-        QuestionDisplay()
+        if (questions != null) {
+            QuestionDisplay(question = questions[0], questionIndex = indexState, viewModel, onNextClicked = {
+                Log.d("Question","${indexState.intValue}")
+            } )
+        }
     }
     Log.d("Size","Questions: ${questions?.size} ")
 
 }
-@Preview(showBackground = true)
+
 @Composable
-fun QuestionDisplay(){
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionViewModel,
+    onNextClicked: (Int) -> Unit,
+)
+{
+    val choicesState = remember(question){
+        question.choices.toMutableList()
+    }
     val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f),0f)
     Surface(modifier =
     Modifier
@@ -64,6 +93,51 @@ fun QuestionDisplay(){
             ) {
                 QuestionTracker()
                 DrawDottedLine(pathEffect = pathEffect)
+                Column(modifier = Modifier
+                    .padding(15.dp)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                ) {
+                    Text(text = "What is this Question ?",
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .align(alignment = Alignment.Start)
+                            .fillMaxHeight(0.3f)
+                        ,
+
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 22.sp,
+                        color = Color.White
+                    )
+                    choicesState.forEachIndexed { index, choice ->
+                        Row(modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(AppColors.mDarkPurple, AppColors.mDarkPurple)
+                                ),
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50,
+                                    bottomStartPercent = 50
+                                )
+                            )
+                            .background(color = Color.Transparent)
+                        ) {
+
+                        }
+                    }
+
+                }
         }
     }
 }
